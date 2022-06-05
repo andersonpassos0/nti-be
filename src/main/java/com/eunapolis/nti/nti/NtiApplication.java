32de091c52914d41029e49ef96308b3b7c227994
andersonpassos0 <anderson.passos@yahoo.com.br>
2022-06-05 00:04:51 -0300

    init commit

---
+++ src/main/java/com/eunapolis/nti/nti/NtiApplication.java
@@ -0,0 +1,13 @@
+package com.eunapolis.nti.nti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eunapolis/nti/nti/NtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eunapolis.nti.nti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
